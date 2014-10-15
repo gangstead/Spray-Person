6ca6c20a454dfd3ef7b438eeb61061c29d5665fc
Steve Gangstead <steven@gangstead.com>
2014-10-15 12:11:21 -0500

    tracking dist folder so app works out of box, instructions to change it put in tutorial and readme

--- src/main/scala/com/example/actors/routes/ApiRouterActor.scala
+++ src/main/scala/com/example/actors/routes/ApiRouterActor.scala
@@ -19,8 +19,10 @@ object ApiRouterActor {
  *
  * Other routes are assumed to be static resources and are served from the resource
  * directory on the classpath.  getFromResourceDirectory takes the remainder of the path
- * so a route like "index.html" is completed with the classpath resource "app/index.html"
+ * so a route like "index.html" is completed with the classpath resource "dist/index.html"
  * or returns a 404 if it's not found.
+ *
+ * To run the front end app in dev mode change "dist" to "app"
  */
 class ApiRouterActor(personRoute: ActorRef) extends Actor
   with HttpService
@@ -32,7 +34,7 @@ class ApiRouterActor(personRoute: ActorRef) extends Actor
       pathPrefix("api") {
         pathPrefix("person") { ctx => personRoute ! ctx }
       } ~
-        getFromResourceDirectory("app")
+        getFromResourceDirectory("dist")
     }
 
   }
